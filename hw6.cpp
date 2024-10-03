f47c39ca29823ce73629cc595c4445ad1a039fa4
nhimsel <>
2024-10-03 11:03:21 -0500

    part 2 completed

--- hw6.cpp
+++ hw6.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 void isVowel(char c);
+int addition(int i, int n);
 
 int main()
 {
-    isVowel('a');
-    isVowel('b');
+    cout << addition(2,3);
     return 0;
 }
 
@@ -15,4 +15,9 @@ void isVowel(char c)
 {
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') cout << "Vowel" << endl;
     else cout << "Not a vowel" << endl;
+}
+
+int addition(int i, int n)
+{
+    return (i+n);
 }
